d0a5c2b36f23e90b46829757a0f4fc4e10a9a17f
Abel Candia <abelcandiach@gmail.com>
2023-04-21 19:47:33 -0400

    added category mvc and reference category to products

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require 'csv'
+# require 'faker'
+
+puts 'Importing categories 4 ...'
+CSV.foreach(Rails.root.join('db/seeds/categories.csv'), headers: true) do |row|
+  Category.create! do |category|
+    category.id = row[0]
+    category.name = row[1]
+  end
+end
+
+# puts 'Importing cities 20...'
+# 20.times do
+#   city = City.new(
+#     name: Faker::Address.city,
+#   )
+#   city.save!
+# end
